98cf0f415bd268017cb64d6c7dc981ca1aee84e3
riad <baornr8@gmail.com>
2021-05-06 07:12:30 +0300

    COMMIT

--- script.js
+++ script.js
@@ -1,6 +1,29 @@
+function updateRate(){
+    var rateval = document.getElementById("rate").value;
+    document.getElementById("rate_val").innerText=rateval;
+}
+
+
 function compute()
 {
-    p = document.getElementById("principal").value;
+    var principal = document.getElementById("principal").value;
+    var rate = document.getElementById("rate").value;
+    
+    var years = document.getElementById("years").value;
+    var interest = principal * years * rate / 100;
+    var year = new Date().getFullYear()+parseInt(years);
+    document.getElementById("result").innerText="If you deposit" + principal
+    +  "\n  at an interest rate of " + rate + " \n You will receive an amount of " + interest
+    + "\n  in the year" + year;
     
 }
-        
+
+function validation(){
+    var x = document.getElementById("principal");
+    if (x.value<=0){
+        alert("Enter a positive number");
+        x.focus();
+        x.select();
+
+    }
+}
